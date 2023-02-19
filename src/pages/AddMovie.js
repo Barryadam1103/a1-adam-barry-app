56e4b13b06865dadd41a671c7d7c8edee9991ef0
Barryadam1103 <barryadam218@gmail.com>
2023-02-19 17:32:59 -0330

    Added image to be selected

--- src/pages/AddMovie.js
+++ src/pages/AddMovie.js
@@ -7,7 +7,7 @@ const AddMovie = (props) => {
         const moviereleaseDate = useRef();
         const movieActors = useRef();
         const movieRating = useRef();
-        //const moviePoster = useRef();
+        const moviePoster = useRef();
     
     
         const submit = (event) => {
@@ -22,8 +22,8 @@ const AddMovie = (props) => {
             const releaseDate = moviereleaseDate.current.value;
             const actors = movieActors.current.value;
             const rating = movieRating.current.value;
-            //const poster = moviePoster.current.value;
-            movieInfo.push({"name":name, "releaseDate":releaseDate, "actors":actors, "rating":rating})
+            const poster = moviePoster.current.value;
+            movieInfo.push({"name":name, "releaseDate":releaseDate, "actors":actors, "rating":rating, "poster":poster})
             props.setMovies(movieInfo);
            
 
@@ -31,7 +31,7 @@ const AddMovie = (props) => {
             moviereleaseDate.current.value = "";
             movieActors.current.value = "";
             movieRating.current.value = "";
-            //moviePoster.current.value = ""
+            moviePoster.current.value = ""
     
     
     
@@ -43,21 +43,23 @@ const AddMovie = (props) => {
                 
                 <label>Movie Name: <input ref = {movieName}type = "text" required/></label>
                 <div>
-                    <label>Movie Release Date: <input ref={moviereleaseDate} type = "text"/></label>
+                    <label>Movie Release Date: <input ref={moviereleaseDate} type = "text" required/></label>
                 </div>
                 <div>
-                    <label>Movie Actors: <input ref={movieActors} type = "text"/></label>
+                    <label>Movie Actors: <input ref={movieActors} type = "text" required/></label>
                 </div>
                 <div>
-                    <label>Movie Rating: <input ref={movieRating} type = "text"/></label>
+                    <label>Movie Rating: <input ref={movieRating} type = "text" required/></label>
+                </div>
+                <div>
+                <label>Choose a Movie Poster</label>
+                            <select ref={moviePoster}>
+                                <option value="././Shrek_(2001_animated_feature_film).jpg">Shrek</option>
+                                {/* <option value="././Mightyducksposter.jpg">Mighty Ducks</option>
+                                <option value="../../StepbrothersMP08.jpg">Step Brothers</option> */}
+                            </select>
                 </div>
                 <input type="submit" value="Submit" />
-
-
-
-
-
-
 
             </form>            
             </>
